d6610757c7137e89a27dbb5e18597b590d07ec6e
Mykyta <mykyta.syskov@seznam.cz>
2023-03-15 22:55:54 +0100

    alternate input

--- GC_Field.cs
+++ GC_Field.cs
@@ -5,30 +5,47 @@ using System.Collections.Generic;
 
 public partial class GC_Field : GridContainer
 {
-	FieldTile[] Field = new FieldTile[25];
+	FieldTile[,] Field = new FieldTile[5,5];
 	// Called when the node enters the scene tree for the first time.
 	bool isPlayer_x = true;
 	public override void _Ready()
 	{
-		for (int i = 0; i < Field.Length; i++)
+		for (int i = 0; i < Field.GetLength(0); i++)
 		{
-			FieldTile ft = new FieldTile(){
+			for (int j = 0; j < Field.GetLength(1); j++)
+			{
+				FieldTile ft = new FieldTile(){
 				SizeFlagsHorizontal = SizeFlags.ExpandFill,
+				SizeFlagsVertical = SizeFlags.ExpandFill,
+				Scale = new Vector2(0.05f, 0.05f),
 				Visible = true,
+				TextureNormal = ResourceLoader.Load("res://btn_empty.png") as Texture2D,
+				IgnoreTextureSize = true,
+				StretchMode = TextureButton.StretchModeEnum.Scale,
+
+				PosX = i,
+				PosY = j,
 			};
 			ft.Pressed += () => TileClicked(ft);
 			AddChild(ft);
+			Field[i,j] = ft;
+			}			
 		}
 	}
 
 	private void TileClicked(FieldTile ft)
 	{
+		ft.PieceExists = true;
 		ft.PlayerIsX = isPlayer_x;
 		GD.Print(isPlayer_x);
 		ft.ActivateTile();
 		isPlayer_x = !isPlayer_x;
+		CheckSurrounding(ft);
 	}
 
+	private void CheckSurrounding(FieldTile ftvar){
+
+	}
 
 	// Called every frame. 'delta' is the elapsed time since the previous frame.
 	public override void _Process(double delta)
