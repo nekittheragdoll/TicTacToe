3fe3eeb5be5ac2413373f841d93fdea72e5ad413
skerikp <107150050+skerikp@users.noreply.github.com>
2023-03-15 15:50:33 +0100

    15.3

--- FieldTile.cs
+++ FieldTile.cs
@@ -6,10 +6,23 @@ namespace TicTacToeResources
     public partial class FieldTile : TextureButton {
 
         public bool PieceExists { get; set; }
-        public bool PieceIsX { get; set; }
+        public bool PlayerIsX { get; set; }
         public int PosX { get; set; }
         public int PosY { get; set; }
         public int Index { get; set; }
+    
+        public void ActivateTile()
+        {
+            if (PlayerIsX)
+            {
+                this.TextureNormal = ResourceLoader.Load("res://x_sprite.png") as Texture2D;
+            }
+            else
+            {
+                this.TextureNormal = ResourceLoader.Load("res://o_sprite.png") as Texture2D;
+            }
+           
+        }
     }
     
 }
--- GC_Field.cs
+++ GC_Field.cs
@@ -7,16 +7,28 @@ public partial class GC_Field : GridContainer
 {
 	FieldTile[] Field = new FieldTile[25];
 	// Called when the node enters the scene tree for the first time.
+	bool isPlayer_x = true;
 	public override void _Ready()
 	{
 		for (int i = 0; i < Field.Length; i++)
 		{
 			FieldTile ft = new FieldTile(){
-				TextureNormal = ResourceLoader.Load("res://o_sprite.png") as Texture2D,
+				SizeFlagsHorizontal = SizeFlags.ExpandFill,
+				Visible = true,
 			};
+			ft.Pressed += () => TileClicked(ft);
 			AddChild(ft);
 		}
 	}
+
+	private void TileClicked(FieldTile ft)
+	{
+		ft.PlayerIsX = isPlayer_x;
+		GD.Print(isPlayer_x);
+		ft.ActivateTile();
+		isPlayer_x = !isPlayer_x;
+	}
+
 
 	// Called every frame. 'delta' is the elapsed time since the previous frame.
 	public override void _Process(double delta)
