6de12863d9124440cd3a94bc853e1cc97433c9b3
Mykyta <mykyta.syskov@seznam.cz>
2023-03-23 11:48:15 +0100

    Added DisplayServer.WindowSetSize

--- GlobalParameters.cs
+++ GlobalParameters.cs
@@ -12,6 +12,8 @@ public partial class GlobalParameters : Node
 	// Called when the node enters the scene tree for the first time.
 	public override void _Ready()
 	{
+
+		DisplayServer.WindowSetSize(new Vector2I(800, 800));
 	}
 
 	// Called every frame. 'delta' is the elapsed time since the previous frame.
